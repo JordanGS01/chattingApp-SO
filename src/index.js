4710dd30684c0d793ce642965b6afe9a91972b79
leanml03 <lean.montero.leiton@gmail.com>
2022-09-18 16:45:04 -0600

    NavBar

--- src/index.js
+++ src/index.js
@@ -2,14 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import "nes.css/css/nes.min.css";
-import App from './App';
-
-
+//import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {NavegationBar} from './components/Navbar/NavegationBar'
 import { MessagesContainer } from './components/Chat/MessagesContainer';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <NavegationBar />
     <MessagesContainer />
   </React.StrictMode>
 );
