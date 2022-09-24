7ef0d179b0ffebe3a47206546c594460884a2f31
Victor <Victorjulio345@gmail.com>
2022-09-24 15:50:26 -0600

    arreglos

--- src/index.js
+++ src/index.js
@@ -6,14 +6,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './index.css';
 
-import App from './App';
+import ContenedorPrincipal  from "../src/components/ContenedorChats/ContenedorPrincipal"
+// import App from './App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-        <App />
+        <ContenedorPrincipal />
     </BrowserRouter>
   </React.StrictMode>
 );
