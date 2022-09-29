c3bfad7e964398dfb687123c196c761eed0d247e
JordanGS01 <gsjordan01@gmail.com>
2022-09-29 12:22:07 -0600

    Merge AddContactv0.1

--- src/components/Audio/audiorec.jsx
+++ src/components/Audio/audiorec.jsx
@@ -1,5 +1,4 @@
 import RecorderControls from "./recorder-controls";
-import RecordingsList from "./recorder-list";
 import useRecorder from "./hooks/useRecorder";
 
 export const AudioRecorder = ({ user, chatId }) => {
@@ -10,7 +9,6 @@ export const AudioRecorder = ({ user, chatId }) => {
       <section className="voice-recorder">
         <div className="recorder-container">
           <RecorderControls recorderState={recorderState} handlers={handlers} user={user} chatId={chatId} audio={audio} />
-          {/* <RecordingsList audio={audio} user={user} chatId={chatId}/> */}
         </div>
       </section>
     );
